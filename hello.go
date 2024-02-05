9f8f2e7ba9d63fef656600faaeedb51360faa215
mathys <mr.red111111@gmail.com>
2024-02-05 23:00:54 +0100

    Playing around with a Spotify API

--- hello.go
+++ hello.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"fmt"
-	
-	"rsc.io/quote"
+	"net/http"
+
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
 )
 
+var state string
+
 func main() {
-	fmt.Println("Hello, World!")
+	redirectURL := "https://localhost:3000"
+	auth := spotifyauth.New(spotifyauth.WithRedirectURL(redirectURL), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate))
+
+	url := auth.AuthURL(state)
+	fmt.Println(url)
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request, auth *spotifyauth.Authenticator) {
+	token, err := auth.Token(r.Context(), state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusNotFound)
+		return
+	}
+	client := spotify.New(auth.Client(r.Context(), token))
+	user, err := client.CurrentUser(r.Context())
+	if err != nil {
+		http.Error(w, "Couldn't get user", http.StatusNotFound)
+		return
+	}
+	fmt.Println(user.DisplayName)
 }
